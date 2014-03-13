9a1b8c1ce1e23270102323d4d95cbde986cd1012
Sam Liu <genxstylez@gmail.com>
2014-03-13 16:56:08 +0800

    add pagination

--- meow/settings.py
+++ meow/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = (
     'meow',
     'documents',
     'providers',
-    'gunicorn'
+    'gunicorn',
+    'pagination'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'meow.urls'
@@ -131,8 +133,19 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '../templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request"
+)
+
+
 # Site title
-SITE_TILE = 'development'
+SITE_TITLE = 'development'
 
 try:
     from local_settings import *
