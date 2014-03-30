523446fbb8737111edb2440b33cfafdae4366f13
Sam Liu <genxstylez@gmail.com>
2014-03-30 17:08:33 +0800

    add category to admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from documents.models import Document
+from documents.models import Document, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
 
 
 class DocumentsAdmin(admin.ModelAdmin):
     list_display = ('provider', 'title', 'href')
 
 admin.site.register(Document, DocumentsAdmin)
+admin.site.register(Category, CategoryAdmin)
