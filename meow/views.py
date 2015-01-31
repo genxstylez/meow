024188711e2a56af31f0904888eaf450f44bd726
Sam Liu <sam@empor.com.tw>
2015-01-31 05:25:29 +0000

    fixes YJ

--- meow/views.py
+++ meow/views.py
@@ -12,7 +12,7 @@ from documents.models import Document, Category
 @cache_page(60 * 15)
 def index(request):
     categories = Category.objects.order_by('-document')[:10]  # Sort by number of document associated
-    documents = Document.objects.order_by('-last_modified')[:1000]
+    documents = Document.objects.order_by('-last_modified', '-views')[:1000]
     '''
     paginator = Paginator(documents, 25)
 
